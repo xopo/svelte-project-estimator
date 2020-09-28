01bcd38059f25d84ffd4edf12ab18e853cabb71b
Daniel <opodaniel@yahoo.com>
2020-09-28 20:19:18 +0300

    - start project, add font-awesome, sscaffold library

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
 <script>
-	export let name;
+	import Table from './Table.svelte';
+	import Form from './Form.svelte';
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		flex: 1;
+		align-content: center;
+		justify-content: center;
+		width: 80%;
 		margin: 0 auto;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+<svelte:head>
+	<title>Project Estimator</title>
+</svelte:head>
+
+<main>
+	<h1>Project estimator</h1>
+	<Form />
+	<Table />
+</main>
