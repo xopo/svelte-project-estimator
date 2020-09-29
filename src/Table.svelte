32cc8fa86dc7f6dd778bf1002ba2bf50cc7ddbeb
Daniel <opodaniel@yahoo.com>
2020-09-29 11:48:05 +0300

    - click on row allow row edit - click cancel clear edit element => add new element - use store functionality in 2 different components

--- src/Table.svelte
+++ src/Table.svelte
@@ -3,34 +3,63 @@
 	import materials from './store';
 	
 	let items = [];
-	const unsubscribe = materials.subscribe(val => {
-		items = [...val];
-	})
+    
+    const unsubscribe = materials.subscribe(itms => {
+		items = [...itms];
+    })
+
+    $: total = items.reduce((total, item) => total += parseFloat(item.price), 0);
+    
     onDestroy(() => unsubscribe);
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    })
+
 </script>
-<main>
-    <table>
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Price</th>
+<style>
+    table {
+        width: 100%;
+    }
+    tfoot {
+        font-weight: bold;
+    }
+    tr {
+        cursor: pointer;
+    }
+    tr:hover {
+        background:  lightyellow;
+    }
+    tr:active {
+        background: gray;
+    }
+</style>
+<table class='primary'>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each items as {id, name, price}, i (id)}
+            <tr on:click|once|preventDefault={materials.setEditItem(id)}>
+                <td>{i+1}</td>
+                <td>{name}</td>
+                <td class='price'>{formatter.format(price.toFixed(2))}</td>
+                <td class='center' on:click|once|preventDefault={()=>materials.remove(id)}>
+                    <i class="far fa-trash-alt"></i>
+                </td>
             </tr>
-        </thead>
-        <tbody>
-            {#each items as {id, name, price}, i (id)}
-                <tr>
-                    <td>{id}</td>
-                    <td>{name}</td>
-                    <td>{price}</td>
-                </tr>
-            {/each}
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="2">Total</td>
-                <td>xxx</td>
-            </tr>
-        </tfoot>
-    </table>
-</main>
+        {/each}
+    </tbody>
+    <tfoot>
+        <tr>
+            <td colspan="2">Total</td>
+            <td>{formatter.format(total)}</td>
+        </tr>
+    </tfoot>
+</table>
